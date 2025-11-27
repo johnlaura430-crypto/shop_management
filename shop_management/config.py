46a4a664e59e26074c2bf205682ef05aaedc035e
johnlaura430-crypto <johnlaura430@gmail.com>
2025-11-27 15:15:09 +0300

    Delete shop_management directory

--- shop_management/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///shop.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- shop_management/run_shop.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Rebecca\Documents\shop_management"
-start http://localhost:5000
-python app.py
